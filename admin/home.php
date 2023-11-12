aee4a243aecb0407d5d45701543ed19015f47aab
phucnv34459 <phucnvph34459@fpt.edu.vn>
2023-11-12 20:48:30 +0700

    update

---
+++ admin/home.php
@@ -0,0 +1,71 @@
+<?php 
+include "header.php";
+?>
+<!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin Nhóm 10</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">admin</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị tài khoản</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị sản phẩm</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị bình luận</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị đơn hàng</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị bài viết</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị liên hệ</a>
+               </li>
+               <li class="nav-tem">
+                <a href="" class="nav-link">Quản trị banner</a>
+               </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+<!-- Content Wrapper. Contains page content -->
+ <div class="content-warpper"></div>
+  <!-- /.content-wrapper -->
